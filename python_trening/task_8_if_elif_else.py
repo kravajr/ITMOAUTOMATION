9339a44c199c452c00a5a2db1ff10022cc7acc07
Ruslan <oboltuss88@gmail.com>
2023-09-04 21:53:39 +0300

    krava commit 1

---
+++ python_trening/task_8_if_elif_else.py
@@ -0,0 +1,33 @@
+if  num_float > 0:
+    print('положителное число')
+elif num_float == 0:
+    print('ноль')
+else:
+    print("число отрицательное")
+
+
+
+permit_print = True
+
+if num > 7 and permit_print
+    print("numn- положительное число")
+elif not  permit_print
+    print('печать запрещена')
+
+ x=12
+
+бакалавр =1
+магистр = 2
+аспирант =3
+
+if x >= 1 and x <= 4:
+    print("бакалавр")
+elif x in range(5,7):
+    print("магистр")
+
+
+x = 22
+if x in range(-100,101):
+    print('+')
+else:
+    print('-')
