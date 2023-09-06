96d23cfc165db46784adf592c02864f8d0d62951
Ruslan <oboltuss88@gmail.com>
2023-09-06 18:55:13 +0300

    krava commit 1

---
+++ home_work/3_hw.py
@@ -0,0 +1,22 @@
+def num1(x,y):
+    return max(x,y)
+print(num1(10,13))
+
+
+a = 228
+b = 333
+if a - b == 135 or b - a == 135:
+    print("Yes")
+else:
+    print('No')
+
+
+x = 7
+y = 13
+z = 15
+if x > 10 and y > 10 and z > 10
+    print('yes')
+else:
+    print('no')
+
+
